558b5f8bdf234799a94d0b946d577fe8fb0dd221
Leonid27-07 <leonid.perkin@gmail.com>
2023-01-13 23:54:28 +0300

    Написание логики работы первой страницы

--- WpfApp1/mos_v.xaml.cs
+++ WpfApp1/mos_v.xaml.cs
@@ -29,12 +29,72 @@ namespace WpfApp1
         {
             k++;
             p_b.Value = k;
+            P_ot1.Foreground = Brushes.Green;
+            P_ot1.IsEnabled= false;
+            n_ot1.IsEnabled= false;
+            n_ot2.IsEnabled= false;
         }
 
         private void P_ot2_Checked(object sender, RoutedEventArgs e)
         {
             k++;
             p_b.Value = k;
+            P_ot2.Foreground = Brushes.Green;
+            P_ot2.IsEnabled = false;
+            n_ot3.IsEnabled = false;
+            n_ot4.IsEnabled = false;
+        }
+
+        private void n_ot1_Checked(object sender, RoutedEventArgs e)
+        {
+            n_ot1.Foreground = Brushes.Red;
+        }
+
+        private void n_ot2_Checked(object sender, RoutedEventArgs e)
+        {
+            n_ot2.Foreground = Brushes.Red;
+        }
+
+        private void n_ot3_Checked(object sender, RoutedEventArgs e)
+        {
+            n_ot3.Foreground = Brushes.Red;
+        }
+
+        private void n_ot4_Checked(object sender, RoutedEventArgs e)
+        {
+            n_ot4.Foreground = Brushes.Red;
+        }
+
+        private void Button_Click_btn_p1(object sender, RoutedEventArgs e)
+        {
+            if (c_b_p1.IsChecked == true && c_b_p2.IsChecked == true && c_b_p3.IsChecked == true && c_b_p3.IsChecked == true && c_b_p4.IsChecked == true && c_b_p5.IsChecked == true && c_b_n1.IsChecked == false)
+            {
+                c_b_p1.IsEnabled = false;
+                c_b_p2.IsEnabled = false;
+                c_b_p3.IsEnabled = false;
+                c_b_p4.IsEnabled = false;
+                c_b_p5.IsEnabled = false;
+                c_b_n1.IsEnabled = false;
+                btn_p1.IsEnabled = false;
+                c_b_p1.Foreground = Brushes.Green;
+                c_b_p2.Foreground = Brushes.Green;
+                c_b_p3.Foreground = Brushes.Green;
+                c_b_p4.Foreground = Brushes.Green;
+                c_b_p5.Foreground = Brushes.Green;
+                c_b_n1.Foreground = Brushes.Red;
+                k = k + 3;
+                p_b.Value = k;
+            }
+            else
+            {
+                c_b_p1.IsChecked = false;
+                c_b_p2.IsChecked = false;
+                c_b_p3.IsChecked = false;
+                c_b_p4.IsChecked = false;
+                c_b_p5.IsChecked = false;
+                c_b_n1.IsChecked = false;
+            }
+
         }
     }
 }
