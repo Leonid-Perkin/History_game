e5498cd8afb7a62a2a91e84cf3cf376c182ce67a
Leonid27-07 <leonid.perkin@gmail.com>
2023-03-06 12:24:48 +0300

    Закончил написание викторины по курской битве

--- WpfApp1/kr_b_v.xaml.cs
+++ WpfApp1/kr_b_v.xaml.cs
@@ -42,7 +42,12 @@ namespace WpfApp1
             v1_l1.IsEnabled = false;
             v2_l1.IsEnabled = false;
             v3_l1.IsEnabled = false;
-            v4_l1.IsEnabled = false;    
+            v4_l1.IsEnabled = false;
+            if (v == 3)
+            {
+                Level2.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v3_l1_Checked(object sender, RoutedEventArgs e)
@@ -73,6 +78,11 @@ namespace WpfApp1
             v6_l1.IsEnabled = false;
             v7_l1.IsEnabled = false;
             v8_l1.IsEnabled = false;
+            if (v == 3)
+            {
+                Level2.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v7_l1_Checked(object sender, RoutedEventArgs e)
@@ -103,6 +113,11 @@ namespace WpfApp1
             v9_l1.IsEnabled = false;
             v11_l1.IsEnabled = false;
             v12_l1.IsEnabled = false;
+            if (v == 3)
+            {
+                Level2.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v11_l1_Checked(object sender, RoutedEventArgs e)
@@ -114,20 +129,6 @@ namespace WpfApp1
         {
             v12_l1.Foreground = Brushes.Red;
         }
-
-        private void bt1_Click(object sender, RoutedEventArgs e)
-        {
-            if (v == 3)
-            {
-                Level2.IsEnabled = true;
-                v = 0;
-            }
-            else
-            {
-
-            }
-        }
-
         private void v1_l2_Checked(object sender, RoutedEventArgs e)
         {
             v1_l2.Foreground = Brushes.Red;
@@ -151,6 +152,11 @@ namespace WpfApp1
             v4_l2.Foreground = Brushes.Red;
             v2_l2.Foreground = Brushes.Red;
             v1_l2.Foreground = Brushes.Red;
+            if (v == 3)
+            {
+                Level3.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v4_l2_Checked(object sender, RoutedEventArgs e)
@@ -171,6 +177,11 @@ namespace WpfApp1
             v6_l2.Foreground = Brushes.Red;
             v7_l2.Foreground = Brushes.Red;
             v8_l2.Foreground = Brushes.Red;
+            if (v == 3)
+            {
+                Level3.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v6_l2_Checked(object sender, RoutedEventArgs e)
@@ -201,6 +212,11 @@ namespace WpfApp1
             v10_l2.IsEnabled = false;
             v11_l2.IsEnabled = false;
             v12_l2.IsEnabled = false;
+            if (v == 3)
+            {
+                Level3.IsEnabled = true;
+                v = 0;
+            }
         }
 
         private void v9_l2_Checked(object sender, RoutedEventArgs e)
@@ -218,17 +234,92 @@ namespace WpfApp1
             v12_l2.Foreground = Brushes.Red;
         }
 
-        private void bt2_Click(object sender, RoutedEventArgs e)
+        private void v1_l3_Checked(object sender, RoutedEventArgs e)
         {
-            if (v == 3)
-            {
-                Level3.IsEnabled = true;
-                v = 0;
-            }
-            else
-            {
+            v1_l3.Foreground = Brushes.Red;
+        }
 
-            }
+        private void v2_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v2_l3.Foreground = Brushes.Red;
+        }
+
+        private void v3_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            o++;
+            p_b.Value = o;
+            v3_l3.Foreground = Brushes.Green;
+            v3_l3.IsEnabled = false;
+            v1_l3.IsEnabled = false;
+            v2_l3.IsEnabled = false;
+            v4_l3.IsEnabled = false;
+            v1_l3.Foreground = Brushes.Red;
+            v2_l3.Foreground = Brushes.Red;
+            v4_l3.Foreground = Brushes.Red;
+
+        }
+
+        private void v4_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v4_l3.Foreground = Brushes.Red;
+        }
+
+        private void v5_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v5_l3.Foreground = Brushes.Red;
+        }
+
+        private void v6_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            o++;
+            p_b.Value = o;
+            v6_l3.Foreground = Brushes.Green;
+            v5_l3.Foreground = Brushes.Red;
+            v7_l3.Foreground = Brushes.Red;
+            v8_l3.Foreground = Brushes.Red;
+            v6_l3.IsEnabled = false;
+            v7_l3.IsEnabled = false;
+            v8_l3.IsEnabled = false;
+            v5_l3.IsEnabled = false;
+        }
+
+        private void v7_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v7_l3.Foreground = Brushes.Red;
+        }
+
+        private void v8_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v8_l3.Foreground = Brushes.Red;
+        }
+
+        private void v9_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v9_l3.Foreground = Brushes.Red;
+        }
+
+        private void v10_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            o++;
+            p_b.Value = o;
+            v9_l3.Foreground = Brushes.Red;
+            v11_l3.Foreground = Brushes.Red;
+            v12_l3.Foreground = Brushes.Red;
+            v10_l3.Foreground = Brushes.Green;
+            v9_l3.IsEnabled = false;
+            v11_l3.IsEnabled = false;
+            v12_l3.IsEnabled = false;
+            v10_l3.IsEnabled = false;
+        }
+
+        private void v11_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v11_l3.Foreground = Brushes.Red;
+        }
+
+        private void v12_l3_Checked(object sender, RoutedEventArgs e)
+        {
+            v12_l3.Foreground = Brushes.Red;
         }
     }
 }
