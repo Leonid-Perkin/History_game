25a5f924ed6408bc97fc18e6fa28b6768fa29490
Leonid <leonid.perkin@gmail.com>
2023-05-21 20:52:40 +0300

    Курская битва карта fix

--- WpfApp1/kr_b_k.xaml.cs
+++ WpfApp1/kr_b_k.xaml.cs
@@ -24,32 +24,52 @@ namespace WpfApp1
             InitializeComponent();
         }
         int p = 0;
+        private void ch_Winner()
+        {
+            if (p == 3)
+            {
+                Winner popup = new Winner();
+                popup.ShowDialog();
+            }
+        }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (ot1.Text== "сорок четвертом")
+            if (ot1.Text== "сорок четвертом" || ot1.Text == "сорок четвёртом" || ot1.Text == "44")
             {
                 p++;
                 p1.Value = p;
+                ot1.Background = Brushes.Green;
+                ot1.Foreground = Brushes.White;
+                ot1.IsEnabled = false;
             }
+            ch_Winner();
         }
 
         private void ot2_TextChanged(object sender, TextChangedEventArgs e)
         {
 
-            if (ot2.Text == "Кольцо")
+            if (ot2.Text == "Кольцо" || ot2.Text == "кольцо")
             {
                 p++;
                 p1.Value = p;
+                ot2.Background = Brushes.Green;
+                ot2.Foreground = Brushes.White;
+                ot2.IsEnabled = false;
             }
+            ch_Winner();
         }
 
         private void ot3_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (ot3.Text == "5")
+            if (ot3.Text == "5" || ot3.Text == "пять")
             {
                 p++;
                 p1.Value = p;
+                ot3.Background = Brushes.Green;
+                ot3.Foreground = Brushes.White;
+                ot3.IsEnabled = false;
             }
+            ch_Winner();
         }
         private void re_Click(object sender, RoutedEventArgs e)
         {
